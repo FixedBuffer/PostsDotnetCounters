2ca09c7016cd0ac7277fddc7e565b77421fe8211
JorTurFer <jorge_turrado@hotmail.es>
2020-04-05 23:28:38 +0200

    initial commit

---
+++ PostContadores/EventSources/ExampleEventSource.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+
+namespace PostContadores.EventSources
+{
+    public class ExampleEventSource : EventSource
+    {
+        private readonly EventCounter _eventCounter;
+        private readonly PollingCounter _poolingCounter;
+        private readonly IncrementingEventCounter _incrementingCounter;
+        private readonly IncrementingPollingCounter _incrementingPollingCounter;
+        private ulong _executions;
+        public ExampleEventSource() : base("Example.Fixedbuffer")
+        {
+            _eventCounter = new EventCounter("event-counter", this)
+            {
+                DisplayUnits = "elements",
+                DisplayName = "EventCounter"
+            };
+
+            _incrementingCounter = new IncrementingEventCounter("incrementing-counter", this)
+            {
+                DisplayUnits = "elements/s",
+                DisplayName = "IncrementingEventCounter",
+            };
+
+            _poolingCounter = new PollingCounter("pooling-counter", this, () => _executions)
+            {
+                DisplayUnits = "elements",
+                DisplayName = "PollingCounter"
+            };
+
+            _incrementingPollingCounter = new IncrementingPollingCounter("incrementing-pooling-counter", this, () => _executions)
+            {
+                DisplayUnits = "elements/s",
+                DisplayName = "IncrementingPollingCounter",
+            };
+        }
+
+        public void Increment()
+        {
+            _executions++;
+            _eventCounter.WriteMetric(_executions);
+            _incrementingCounter.Increment();
+        }
+    }
+}
---
+++ PostContadores/Program.cs
@@ -0,0 +1,19 @@
+﻿using PostContadores.EventSources;
+using System.Threading;
+
+namespace PostContadores
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var exampleEventSource = new ExampleEventSource();
+
+            for (var i = 0; i < 3000; i++)
+            {
+                exampleEventSource.Increment();
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
